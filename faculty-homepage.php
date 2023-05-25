2abf060e387152bca2e45a519a55a579fb60c1c8
Akintuntun <119497959+Akintuntun@users.noreply.github.com>
2023-05-25 19:33:33 +0800

    Faculty Home Page (start)

--- faculty-homepage.php
+++ faculty-homepage.php
@@ -3,10 +3,44 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FACULTY HOMEPAGE</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@900&display=swap" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <title>LOGIN PAGE</title>
+    <link rel="stylesheet" href="faculty.css">
 </head>
 <body>
+<header>
+        <div class="header">
+            <img src="images/lspu-logo.png" alt="LSPU-LOGO" class="logo">
+            <div class="label">
+                <h1 class="title"><i>Laguna State Polytechnic University San Pablo City Campus</i></h1>
+                <h2 class="college">College of Computer Studies</h2>
+            </div>
+            <img src="images\ccs.png" alt="CCS-LOGO" class="logo">
+        </div>
+        <div class="head-w">
+            <img src="images\opt.png" alt="" class="opt">
+            <h2>Juan Dela Cruz</h2>
+        </div>
+    </header>
     
+    <div class="main-body">
+        <div class="menu">
+            <ul class="options">
+                <li>Dashboard</li>
+                <li>IPCR</li>
+                <li>Notification</li>
+                <li>System Feedback</li>
+            </ul>
+        </div>
+        <div class="frame">
+            <h1>afsd</h1>
+        </div>
+    </div>
+
 </body>
 </html>
