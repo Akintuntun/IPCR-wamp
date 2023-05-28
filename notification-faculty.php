c7f707130c06dbf7d9591f757c8e24ad98367ecc
Akintuntun <119497959+Akintuntun@users.noreply.github.com>
2023-05-28 22:31:13 +0800

    NOTI

--- notification-faculty.php
+++ notification-faculty.php
@@ -4,9 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS\notification.css">
     <title>Document</title>
 </head>
 <body>
-    
+    <div class="container">
+        <h1>Message</h1>
+        <h2></h2>
+        <h1>List of Deficiencies</h1>
+        <h2></h2>
+    </div>
 </body>
 </html>
