aa21b17c49a4239f83fff07692020b6681b77a22
Akintuntun <119497959+Akintuntun@users.noreply.github.com>
2023-05-29 20:22:47 +0800

    update dashboard

--- dashboard-faculty.php
+++ dashboard-faculty.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS\dashboard-faculty.css">
     <title>Document</title>
 </head>
 
@@ -15,6 +16,31 @@
 
 </style>
 
+
 <body>
+
+    <div class="container">
+        <h1 style="text-align: center;">Employee Performance Dashboard</h1>
+        <div class="body">
+
+            <div class="profile">
+                <img src="images/account-logo.png" alt="missing" class="opt">
+                <div class="progress-bar">
+                    <div class="progress" style="width: <?php echo 76; ?>%;">
+                    </div>
+                </div>
+                <h1>Juan Dela Cruz</h1>
+            </div>
+            <div class="bar-achievements">
+                <h1>fasd</h1>
+            </div>
+            <div class="bar-skills">
+                <h1>fasd</h1>
+            </div>
+
+
+        </div>
+    </div>
+
 </body>
 </html>
